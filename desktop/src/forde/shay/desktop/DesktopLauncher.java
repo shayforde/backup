7794ea0f492b8ce08450fa5489071bbb32f24561
shayforde <shayforde@gmail.com>
2014-05-26 21:06:20 +0100

    first commit

---
+++ desktop/src/forde/shay/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package forde.shay.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import forde.shay.sfGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new sfGdxGame(), config);
+	}
+}
